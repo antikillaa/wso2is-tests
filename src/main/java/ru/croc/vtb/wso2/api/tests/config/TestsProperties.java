ba435ef8ad0c7c4b545e6628b1701cb3c4438f4d
APleshchenkov <apleshchenkov@croc.ru>
2020-05-20 11:12:27 +0300

    fix test properties evaluator

--- src/main/java/ru/croc/vtb/wso2/api/tests/config/TestsProperties.java
+++ src/main/java/ru/croc/vtb/wso2/api/tests/config/TestsProperties.java
@@ -5,12 +5,14 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
 @Configuration
 @ConfigurationProperties(prefix = "test")
+@PropertySource("application.properties")
 @Component
 public class TestsProperties {
 
