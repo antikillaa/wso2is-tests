f1a729e0e3057210ca73ab7d187e3ddb7c205b1b
Пешкур Станислав Иванович (4033838) <peshkur@vtb.ru>
2021-02-04 14:58:38 +0300

    Added start tests from Maven

--- src/main/java/ru/croc/vtb/wso2/api/tests/RunCucumberTest.java
+++ src/main/java/ru/croc/vtb/wso2/api/tests/RunCucumberTest.java
@@ -2,14 +2,20 @@ package ru.croc.vtb.wso2.api.tests;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.restassured.RestAssured;
 import org.junit.runner.RunWith;
+import org.junit.runners.model.InitializationError;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        publish = true,
         features = "classpath:features",
         plugin = {"pretty", "html:test-report/cucumber-report.html"},
         tags = "@wip"
 )
 public class RunCucumberTest {
+
+    public static void main(String[] args) throws InitializationError {
+        RestAssured.useRelaxedHTTPSValidation();
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
 }
