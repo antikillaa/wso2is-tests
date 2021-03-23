2d879d0e84db50fc746f4214faefe8ae0d75cf3a
Stanislav Peshkur <stanislav@samantree.com>
2021-03-22 22:51:52 -0700

    Fix partner SSO port

--- src/main/java/ru/croc/vtb/wso2/api/tests/impl/request/PartnerSSORequestServiceImpl.java
+++ src/main/java/ru/croc/vtb/wso2/api/tests/impl/request/PartnerSSORequestServiceImpl.java
@@ -199,7 +199,7 @@ public class PartnerSSORequestServiceImpl implements PartnerSSORequestService {
                     break;
             }
         }
-        return URL + ":9090/oauth2/" + path;
+        return URL + ":" + testsProperties.getWsoPort() + "/oauth2/" + path;
     }
 
 
